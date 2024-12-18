59c42f2048df52a797d2f4dce2dd64def6b9f4e7
ihya <ihyamars@gmail.com>
2024-12-18 12:53:12 +0700

    add php IDE Helper

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if ($this->app->environment('APP_ENV') == 'local') {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
     }
 
     /**
