10c19f6cc90cf81c6ea29ace1b1276880fd12629
ihya <ihyamars@gmail.com>
2025-09-22 08:14:48 +0700

    update migrations

--- database/migrations/2024_09_13_121214_create_registration_data_table.php
+++ database/migrations/2024_09_13_121214_create_registration_data_table.php
@@ -12,12 +12,29 @@ return new class () extends Migration {
     {
         Schema::create('registration_data', function (Blueprint $table) {
             $table->id();
+
+            ## Sales
             $table->enum('type', ['apps','anbk','snbt']);
             $table->enum('periode', ['Januari - Juni', 'Juli - Desember']);
+            $table->string('school_years');
             $table->date('date_register');
             $table->string('provinces');
             $table->string('regencies');
             $table->integer('student_count');
+            $table->string('counselor_coordinators');
+            $table->string('counselor_coordinators_phone')->nullable();
+            $table->string('curriculum_deputies');
+            $table->string('curriculum_deputies_phone')->nullable();
+            $table->string('proctors');
+            $table->string('proctors_phone')->nullable();
+            $table->string('schools');
+            $table->string('class')->nullable();
+            $table->string('education_level');
+            $table->string('principal');
+            $table->string('principal_phone')->nullable();
+            $table->enum('education_level_type', ['Negeri','Swasta']);
+            
+            ## Akademik dan Teknisi
             $table->date('implementation_estimate');
             $table->date('group')->nullable();
             $table->date('bimtek')->nullable();
@@ -29,6 +46,8 @@ return new class () extends Migration {
             $table->enum('pm', ['ya', 'tidak'])->nullable();
             $table->date('counselor_consultation_date')->nullable();
             $table->date('student_consultation_date')->nullable();
+            
+            ## Finance
             $table->string('price')->nullable();
             $table->string('total')->nullable();
             $table->string('net')->nullable();
@@ -38,18 +57,47 @@ return new class () extends Migration {
             $table->date('payment_date')->nullable();
             $table->string('payment')->nullable();
             $table->string('cb')->nullable();
-            $table->string('schools');
-            $table->string('education_level');
-            $table->string('principal');
-            $table->string('phone_principal')->nullable();
-            $table->enum('education_level_type', ['Negeri','Swasta']);
+            $table->string('option_price')->nullable();
+            $table->string('monthYear')->nullable();
 
 
-            $table->foreignId('curriculum_deputies_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
-            $table->foreignId('counselor_coordinators_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
-            $table->foreignId('proctors_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->string('sudin')->nullable();
+
+            $table->string('mitra_difference')->nullable();
+            $table->string('mitra_net')->nullable();
+            $table->string('mitra_subtotal')->nullable();
+
+            $table->string('ss_difference')->nullable();
+            $table->string('ss_net')->nullable();
+            $table->string('ss_subtotal')->nullable();
+
+            $table->string('dll_difference')->nullable();
+            $table->string('dll_net')->nullable();
+            $table->string('dll_subtotal')->nullable();
+
+            $table->string('detail_invoice')->nullable();
+            $table->string('number_invoice')->nullable();
+            $table->string('qty_invoice')->nullable();
+            $table->string('unit_price')->nullable();
+            $table->string('amount_invoice')->nullable();
+            $table->string('tax_rate')->nullable();
+            $table->string('sales_tsx')->nullable();
+            $table->string('other')->nullable();
+            $table->string('subtotal_invoice')->nullable();
+            $table->string('total_invoice')->nullable();
+
+            $table->string('detail_kwitansi')->nullable();
+            $table->string('difference_total')->nullable();
+
+            $table->string('subtotal_1')->nullable();
+            $table->string('subtotal_2')->nullable();
+
+            $table->string('net_2')->nullable();
+            $table->string('student_count_1')->nullable();
+            $table->string('student_count_2')->nullable();
+
+
             $table->foreignId('users_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
-            $table->foreignId('school_years_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
         });
     }
