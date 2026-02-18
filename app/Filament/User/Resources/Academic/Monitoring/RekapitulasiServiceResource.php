ecfc1eb8457257e8d9fbe660cbfe26d93407c411
ihyamars <ihyamars@gmail.com>
2026-02-18 12:40:05 +0700

    update filter

--- app/Filament/User/Resources/Academic/Monitoring/RekapitulasiServiceResource.php
+++ app/Filament/User/Resources/Academic/Monitoring/RekapitulasiServiceResource.php
@@ -17,13 +17,13 @@ class RekapitulasiServiceResource extends Resource
 {
     protected static ?string $model = RegistrationData::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+    protected static ?string $navigationIcon = "heroicon-o-document-text";
 
-    protected static ?string $navigationGroup = 'Service';
+    protected static ?string $navigationGroup = "Service";
 
-    protected static ?string $navigationLabel = 'Rekapitulasi';
+    protected static ?string $navigationLabel = "Rekapitulasi";
 
-    protected static ?string $modelLabel = 'rekapitulasi';
+    protected static ?string $modelLabel = "rekapitulasi";
 
     public static function canViewAny(): bool
     {
@@ -35,57 +35,143 @@ class RekapitulasiServiceResource extends Resource
         return $table
             ->paginated([50, 100, 200])
             ->columns([
-                TextColumn::make('row_number')
-                    ->label('No')
+                TextColumn::make("row_number")
+                    ->label("No")
                     ->alignCenter()
-                    ->getStateUsing(fn ($rowLoop) => $rowLoop->iteration),
-                TextColumn::make('schools')
-                    ->label('Schools')
-                    ->searchable(),
-                IconColumn::make('group')
-                    ->label('GRUP')
+                    ->getStateUsing(fn($rowLoop) => $rowLoop->iteration),
+                TextColumn::make("schools")->label("Schools")->searchable(),
+                IconColumn::make("group")
+                    ->label("GRUP")
                     ->alignCenter()
-                    ->icon(fn ($record) => ($record->latestStatusLog?->status?->order ?? 0) >= 3 ? 'heroicon-s-check' : 'heroicon-s-x-mark')
-                    ->color(fn ($record) => ($record->latestStatusLog?->status?->order ?? 0) >= 3 ? 'success' : 'danger')
+                    ->icon(
+                        fn($record) => ($record->latestStatusLog?->status
+                            ?->order ??
+                            0) >=
+                        3
+                            ? "heroicon-s-check"
+                            : "heroicon-s-x-mark",
+                    )
+                    ->color(
+                        fn($record) => ($record->latestStatusLog?->status
+                            ?->order ??
+                            0) >=
+                        3
+                            ? "success"
+                            : "danger",
+                    )
                     ->default(false),
-                IconColumn::make('bimtek')
-                    ->label('BIMTEK')
+                IconColumn::make("bimtek")
+                    ->label("BIMTEK")
                     ->alignCenter()
-                    ->icon(fn ($record) => ($record->latestStatusLog?->status?->order ?? 0) >= 4 ? 'heroicon-s-check' : 'heroicon-s-x-mark')
-                    ->color(fn ($record) => ($record->latestStatusLog?->status?->order ?? 0) >= 4 ? 'success' : 'danger')
+                    ->icon(
+                        fn($record) => ($record->latestStatusLog?->status
+                            ?->order ??
+                            0) >=
+                        4
+                            ? "heroicon-s-check"
+                            : "heroicon-s-x-mark",
+                    )
+                    ->color(
+                        fn($record) => ($record->latestStatusLog?->status
+                            ?->order ??
+                            0) >=
+                        4
+                            ? "success"
+                            : "danger",
+                    )
                     ->default(false),
-                IconColumn::make('account_count_created')
-                    ->label('AKUN')
+                IconColumn::make("account_count_created")
+                    ->label("AKUN")
                     ->alignCenter()
-                    ->icon(fn ($record) => ($record->latestStatusLog?->status?->order ?? 0) >= 6 ? 'heroicon-s-check' : 'heroicon-s-x-mark')
-                    ->color(fn ($record) => ($record->latestStatusLog?->status?->order ?? 0) >= 6 ? 'success' : 'danger')
+                    ->icon(
+                        fn($record) => ($record->latestStatusLog?->status
+                            ?->order ??
+                            0) >=
+                        6
+                            ? "heroicon-s-check"
+                            : "heroicon-s-x-mark",
+                    )
+                    ->color(
+                        fn($record) => ($record->latestStatusLog?->status
+                            ?->order ??
+                            0) >=
+                        6
+                            ? "success"
+                            : "danger",
+                    )
                     ->default(false),
-                IconColumn::make('implementer_count')
-                    ->label('EVENT')
+                IconColumn::make("implementer_count")
+                    ->label("EVENT")
                     ->alignCenter()
-                    ->icon(fn ($record) => ($record->latestStatusLog?->status?->order ?? 0) >= 7 ? 'heroicon-s-check' : 'heroicon-s-x-mark')
-                    ->color(fn ($record) => ($record->latestStatusLog?->status?->order ?? 0) >= 7 ? 'success' : 'danger')
+                    ->icon(
+                        fn($record) => ($record->latestStatusLog?->status
+                            ?->order ??
+                            0) >=
+                        7
+                            ? "heroicon-s-check"
+                            : "heroicon-s-x-mark",
+                    )
+                    ->color(
+                        fn($record) => ($record->latestStatusLog?->status
+                            ?->order ??
+                            0) >=
+                        7
+                            ? "success"
+                            : "danger",
+                    )
                     ->default(false),
-                IconColumn::make('students_download')
-                    ->label('DOWNLOAD')
+                IconColumn::make("students_download")
+                    ->label("DOWNLOAD")
                     ->alignCenter()
-                    ->icon(fn ($record) => ($record->latestStatusLog?->status?->order ?? 0) >= 9 ? 'heroicon-s-check' : 'heroicon-s-x-mark')
-                    ->color(fn ($record) => ($record->latestStatusLog?->status?->order ?? 0) >= 9 ? 'success' : 'danger')
+                    ->icon(
+                        fn($record) => ($record->latestStatusLog?->status
+                            ?->order ??
+                            0) >=
+                        9
+                            ? "heroicon-s-check"
+                            : "heroicon-s-x-mark",
+                    )
+                    ->color(
+                        fn($record) => ($record->latestStatusLog?->status
+                            ?->order ??
+                            0) >=
+                        9
+                            ? "success"
+                            : "danger",
+                    )
                     ->default(false),
-                IconColumn::make('schools_download')
-                    ->label('PM')
+                IconColumn::make("schools_download")
+                    ->label("PM")
                     ->alignCenter()
-                    ->icon(fn ($record) => ($record->latestStatusLog?->status?->order ?? 0) >= 10 ? 'heroicon-s-check' : 'heroicon-s-x-mark')
-                    ->color(fn ($record) => ($record->latestStatusLog?->status?->order ?? 0) >= 10 ? 'success' : 'danger')
+                    ->icon(
+                        fn($record) => ($record->latestStatusLog?->status
+                            ?->order ??
+                            0) >=
+                        10
+                            ? "heroicon-s-check"
+                            : "heroicon-s-x-mark",
+                    )
+                    ->color(
+                        fn($record) => ($record->latestStatusLog?->status
+                            ?->order ??
+                            0) >=
+                        10
+                            ? "success"
+                            : "danger",
+                    )
                     ->default(false),
-                TextColumn::make('updated_at')
-                    ->label('Terakhir Diupdate')
+                TextColumn::make("updated_at")
+                    ->label("Terakhir Diupdate")
                     ->alignCenter()
-                    ->formatStateUsing(fn ($state) => Carbon::parse($state)->translatedFormat('l, d/m/Y H:i')),
+                    ->formatStateUsing(
+                        fn($state) => Carbon::parse($state)->translatedFormat(
+                            "l, d/m/Y H:i",
+                        ),
+                    ),
             ])
-            ->defaultSort('updated_at', 'desc')
+            ->defaultSort("updated_at", "desc")
             ->filters([
-                //   Tables\Filters\SelectFilter::make("periode")
+                Tables\Filters\SelectFilter::make("periode")
                     ->label("Periode")
                     ->options(Periode::list())
                     ->preload(),
@@ -124,29 +210,35 @@ class RekapitulasiServiceResource extends Resource
 
                         $query->whereHas(
                             "status",
-                            fn(Builder $q) => $q->where("color", $data["value"]),
+                            fn(Builder $q) => $q->where(
+                                "color",
+                                $data["value"],
+                            ),
                         );
                     }),
             ])
-            ->recordAction('view')
-            ->actions([
-                // Tables\Actions\EditAction::make(),
-            ], position: ActionsPosition::BeforeColumns)
+            ->recordAction("view")
+            ->actions(
+                [
+                    // Tables\Actions\EditAction::make(),
+                ],
+                position: ActionsPosition::BeforeColumns,
+            )
             ->bulkActions([]);
     }
 
     public static function getRelations(): array
     {
         return [
-            //
-        ];
+                //
+            ];
     }
 
     public static function getPages(): array
     {
         return [
-            'index' => Pages\ListRekapitulasiServices::route('/'),
-            'view' => Pages\ViewRekapitulasiService::route('/{record}'),
+            "index" => Pages\ListRekapitulasiServices::route("/"),
+            "view" => Pages\ViewRekapitulasiService::route("/{record}"),
         ];
     }
 }
