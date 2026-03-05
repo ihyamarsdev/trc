547c25e84a921e888a2187a556e6b6665a4d0894
ihyamars <ihyamars@gmail.com>
2026-03-05 15:04:51 +0700

    upgrade filament v4

---
+++ app/Filament/Resources/RoleResource.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\Roles\RoleResource as BaseRoleResource;
+
+class RoleResource extends BaseRoleResource {}
