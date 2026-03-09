d480d6cb9060f72762b675305013562013a4a1ee
ihyamars <ihyamars@gmail.com>
2026-03-09 12:52:13 +0700

    style: apply code formatting to RoleResource

--- app/Filament/Resources/RoleResource.php
+++ app/Filament/Resources/RoleResource.php
@@ -50,15 +50,15 @@ class RoleResource extends Resource
                     }),
             ])
             ->filters([
-                    //
+                //
             ])
             ->actions([
-                    Tables\Actions\EditAction::make(),
+                Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
-                    Tables\Actions\BulkActionGroup::make([
-                        Tables\Actions\DeleteBulkAction::make(),
-                    ]),
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
             ]);
     }
 
